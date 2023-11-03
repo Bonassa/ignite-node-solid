f67610b0a515e22aaf62a06401ff1dab71d2d02a
Renan Bonassa <renan.bonassa@wefit.com.br>
2023-11-03 09:15:43 -0300

    Feat: Check-in use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from 'node:crypto'
+
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
@@ -18,7 +20,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user: User = {
-      id: '1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
